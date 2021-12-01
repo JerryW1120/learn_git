c3509b133382c410324eacced6edce5013bf9ef5
Jerry9403 <940341746@qq.com>
2021-12-02 01:53:17 +0800

    做了些删减

--- findmax(4).py
+++ findmax(4).py
@@ -4,34 +4,9 @@ def findmax(n):
     count = 0
 
     while count < n:
-        item = input("请输入元素:")
+        item = int(input("请输入元素:"))
         mylist.append(item)
         count += 1
-
-    int_count = 0
-    str_count = 0
-
-    for item in mylist:
-        if item[0] == '-':
-            item = list(item)
-            item.remove('-')
-            item_abs = "".join(item)
-
-            if item_abs.isdigit():
-                int_count += 1
-
-        else:
-            if item.isdigit():
-                int_count += 1
-            else:
-                str_count += 1
-                break
-
-    if str_count == 0 and int_count == n:
-        count = 0
-        while count < n:
-            mylist[count] = int(mylist[count])
-            count += 1
 
     return mylist
 
